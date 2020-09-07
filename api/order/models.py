30098939ebb3819219108402cc9b2f039edd8ee8
Sergey <ss.vanteev@gmail.com>
2020-09-07 20:48:13 +0300

    added Order for backend

--- api/order/models.py
+++ api/order/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from api.user.models import CustomUser
+from api.product.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    models.ForeignKey(CustomUser, null=True, blank=True, verbose_name=(
+        ""), on_delete=models.CASCADE)
+    product_names = models.CharField(max_length=500)
+    total_products = models.CharField(max_length=500, default=0)
+    transaction_id = models.CharField(max_length=500, default=0)
+    total_amount = models.CharField(max_length=50, default=0)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = ("")
+        verbose_name_plural = ("s")
+
+    def __str__(self):
+        return self.name
--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('category/', include('api.category.urls')),
     path('product/', include('api.product.urls')),
-    path('user/', include('api.user.urls'))
+    path('user/', include('api.user.urls')),
+    path('order/', include('api.order.urls')),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token_auth')
 ]
